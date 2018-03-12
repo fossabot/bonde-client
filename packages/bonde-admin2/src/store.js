71b70f40f46b5f2f09b572034603050778bc6b7f
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-03-12 12:50:04 -0300

    chore(bonde-admin2): available locale strategy function

--- packages/bonde-admin2/src/store.js
+++ packages/bonde-admin2/src/store.js
@@ -1,13 +1,32 @@
 import { createStore, combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
-const currentLocale = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage
+export const locales = ['pt-BR', 'en', 'es']
+export const defaultLocale = 'pt-BR'
+
+export const localeStrategy = locale => {
+  const localeWithoutRegionCode = locale.toLowerCase().split(/[_-]+/)[0]
+
+  if (locales.includes(locale)) {
+    return locale
+  } else if (locales.includes(localeWithoutRegionCode)) {
+    return localeWithoutRegionCode
+  }
+
+  return defaultLocale
+}
+
+const currentLocale = localeStrategy(
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage
+)
 
 const initialState = {
   currentLocale
 }
 
-const intlReducer = (state = 'pt-BR', action) => {
+const intlReducer = (state = defaultLocale, action) => {
   switch (action.type) {
     case 'intl/SET_CURRENT_LOCALE':
       return action.payload
